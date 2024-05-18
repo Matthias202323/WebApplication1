1e3b307da5ad902ea75a9668449618cc45bc9fb5
MatthiasDesmoulins <Matthias-Externe.Desmoulins@asmeg.org>
2024-05-18 16:31:25 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Article.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+       
+        public string? Contenu { get; set; }
+       
+    }
+}
